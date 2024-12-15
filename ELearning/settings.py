083953255295d95940d850a540cda0c4a36dec0f
srikanthkanniyappan <srikanthkanniyappan.r@gmail.com>
2024-12-16 00:33:54 +0530

    Deploy On Vercel

--- ELearning/settings.py
+++ ELearning/settings.py
@@ -200,7 +200,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # CORS_ALLOWED_ORIGINS = ["https://elearningsrikanth.netlify.app"]
 CORS_ALLOWED_ORIGINS = [
-
+'https://elearning-react-ochre.vercel.app'
 ]
 
 # CLOUDINARY Configuration
--- ELearning/wsgi.py
+++ ELearning/wsgi.py
@@ -7,20 +7,22 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
-# import os
-#
-# from django.core.wsgi import get_wsgi_application
-#
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ELearning.settings')
-#
-# application = get_wsgi_application()
-
 import os
 
 from django.core.wsgi import get_wsgi_application
-from static_ranges import Ranges
-from dj_static import Cling, MediaCling
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ELearning.settings')
 
-application = Ranges(Cling(MediaCling(get_wsgi_application())))
+application = get_wsgi_application()
+
+app = application
+
+# import os
+#
+# from django.core.wsgi import get_wsgi_application
+# from static_ranges import Ranges
+# from dj_static import Cling, MediaCling
+#
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ELearning.settings')
+#
+# application = Ranges(Cling(MediaCling(get_wsgi_application())))
